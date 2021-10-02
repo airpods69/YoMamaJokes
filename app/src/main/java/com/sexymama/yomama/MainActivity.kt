df41033ba2f5bccdbce091bb71ff9a6d7144453c
Airpods69 <varenyambhardwaj123@gmail.com>
2021-10-02 11:22:11 +0530

    Add onclick to change joke

--- app/src/main/java/com/sexymama/yomama/MainActivity.kt
+++ app/src/main/java/com/sexymama/yomama/MainActivity.kt
@@ -4,6 +4,7 @@ import android.os.AsyncTask
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.TextView
+import androidx.constraintlayout.widget.ConstraintLayout
 import org.json.JSONObject
 import java.net.URL
 
@@ -11,22 +12,21 @@ import java.net.URL
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-
-        yomama().execute()
+        val viewer = findViewById<ConstraintLayout>(R.id.viewer)
+        viewer.setOnClickListener {
+            yomama().execute()
+        }
     }
 
     inner class yomama() : AsyncTask<String, Void, String>() {
         override fun doInBackground(vararg p0: String?): String {
             var response:String?
             response = URL("https://api.yomomma.info/").readText(Charsets.UTF_8)
-
             return response
-
         }
 
         override fun onPostExecute(result: String?) {
             super.onPostExecute(result)
-
             val jsonObj = JSONObject(result)
             findViewById<TextView>(R.id.jokeText).text = jsonObj.getString("joke")
         }
